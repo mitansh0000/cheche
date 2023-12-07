ebbee86d799c8e002f2bb34b3b93dbcfb563e46a
Mitansh Dhiman <118064821+mitansh0000@users.noreply.github.com>
2023-12-07 09:12:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,24 @@ io.on('connection', (socket) => {
       }
     }
   });
+  socket.on('join', (roomId) => {
+    const selectedRoom = io.sockets.adapter.rooms[roomId]
+    const numberOfClients = selectedRoom ? selectedRoom.length : 0
+
+    // These events are emitted only to the sender socket.
+    if (numberOfClients == 0) {
+      console.log(`Creating room ${roomId} and emitting room_created socket event`)
+      socket.join(roomId)
+      socket.emit('room_created', roomId)
+    } else if (numberOfClients == 1) {
+      console.log(`Joining room ${roomId} and emitting room_joined socket event`)
+      socket.join(roomId)
+      socket.emit('room_joined', roomId)
+    } else {
+      console.log(`Can't join room ${roomId}, emitting full_room socket event`)
+      socket.emit('full_room', roomId)
+    }
+  })
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
@@ -47,34 +65,27 @@ io.on('connection', (socket) => {
   socket.on('setUsername', (username) => {
     socket.username = username;
   });
-});
 // Define a route for the video call page
 app.get('/video-call', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'video-call.html'));
 });
-// Video Call Socket Namespace
-videoCallSockets.on('connection', (socket) => {
-  console.log('A user connected to video call');
-
-  socket.on('join-room', () => {
-    socket.join('video-call-room');
-  });
-
-  socket.on('ice-candidate', (candidate) => {
-    videoCallSockets.to('video-call-room').emit('ice-candidate', candidate);
-  });
-
-  socket.on('offer', (offer) => {
-    videoCallSockets.to('video-call-room').emit('offer', offer);
-  });
-
-  socket.on('answer', (answer) => {
-    videoCallSockets.to('video-call-room').emit('answer', answer);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected from video call');
-  });
+ // These events are emitted to all the sockets connected to the same room except the sender.
+ socket.on('start_call', (roomId) => {
+  console.log(`Broadcasting start_call event to peers in room ${roomId}`)
+  socket.broadcast.to(roomId).emit('start_call')
+})
+socket.on('webrtc_offer', (event) => {
+  console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
+  socket.broadcast.to(event.roomId).emit('webrtc_offer', event.sdp)
+})
+socket.on('webrtc_answer', (event) => {
+  console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
+  socket.broadcast.to(event.roomId).emit('webrtc_answer', event.sdp)
+})
+socket.on('webrtc_ice_candidate', (event) => {
+  console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
+  socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
+})
 });
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
